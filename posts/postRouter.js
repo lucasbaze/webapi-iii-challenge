4ca7d8676f677a520aaf85adae074a08553a67fb
Lucas Bazemore <lucasabazemore@gmail.com>
2019-10-02 13:10:35 -0500

    add get specific post handler

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -14,7 +14,19 @@ router.get('/', async (req, res) => {
     res.status(200).json({ posts: posts });
 });
 
-router.get('/:id', (req, res) => {});
+//
+//Get specifc post
+router.get('/:id', async (req, res) => {
+    let { id } = req.params;
+    let post = await db.getById(id);
+    if (!post || post == null) {
+        res.status(500).json({
+            error: 'There is no post with that ID',
+        });
+        return;
+    }
+    res.status(200).json({ post: post });
+});
 
 router.delete('/:id', (req, res) => {});
 
